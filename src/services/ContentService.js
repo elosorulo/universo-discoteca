d6b5a4b1e4ff1417acb474df91aff8701f329a83
elosorulo <manustrajman@gmail.com>
2020-07-22 21:58:49 -0300

    Fix audio loop.

--- src/services/ContentService.js
+++ src/services/ContentService.js
@@ -4,7 +4,7 @@ import Image3 from '../assets/3.jpg';
 import Image4 from '../assets/4.jpg';
 import Image5 from '../assets/5.jpg';
 import Image6 from '../assets/6.jpg';
-import sound from '../assets/universo-discoteca-sonido-entrega.mp3';
+import sound from '../assets/universo-discoteca-loop.mp3';
 
 const getImages = () => {
     return [Image1, Image2, Image3, Image4, Image5, Image6];
